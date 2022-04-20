7be49a4aa52a7338b06681aadd729b83fcc47acf
magodosmestres <lucasmoraisf@gmail.com>
2022-04-20 08:21:04 -0300

    fill ellipse ready

--- main.c
+++ main.c
@@ -29,10 +29,10 @@ short getInputs(int* pl, int* pc, int* pp) {
     return 1;
 }
 
-void printMatrix(int*** x, int p, int l, int c) {
-    for (int i = 0; i < p; i++) {
+void printMatrix(int*** x, unsigned int p, unsigned int l, unsigned int c) {
+    for (int k = 0; k < p; k++) {
         for (int j = 0; j < l; j++) {
-            for (int k = 0; k < c; k++){
+            for (int i = 0; i < c; i++){
                 printf("%d ", x[i][j][k]);
             }
             printf("\n");
@@ -42,9 +42,9 @@ void printMatrix(int*** x, int p, int l, int c) {
 }
 
 void getVals(int*** x, int p, int l, int c) {
-    for (int i = 0; i < p; i++) {
+    for (int i = 0; i < c; i++) {
         for (int j = 0; j < l; j++) {
-            for (int k = 0; k < c; k++){
+            for (int k = 0; k < p; k++){
                 printf("entrada [%d][%d][%d] ", i, j, k);
                 scanf("%d", &x[i][j][k]);
             }
@@ -52,29 +52,61 @@ void getVals(int*** x, int p, int l, int c) {
     }
 }
 
-int main() {
+void fillMatrix(int*** x, unsigned int rx, unsigned int ry, unsigned int rz) {
 
     unsigned int p, l, c;
+    p = 2*rz + 1;
+    l = 2*ry + 1;
+    c = 2*rx + 1;
 
-    if (getInputs(&p, &l, &c) == 0) {
+    float u, v, w;
+    u = (float)rx;
+    v = (float)ry;
+    w = (float)rz;
+
+    for (int k = 0; k < p; k++) {
+        for (int j = 0; j < l; j++) {
+            for (int i = 0; i < c; i++){
+                if ((i - u)*(i - u)/(u*u) + (j - v)*(j - v)/(v*v) + (k - w)*(k - w)/(w*w) <= 1.) {
+                    x[i][j][k] = 1;
+                } else {
+                    x[i][j][k] = 0;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+
+    unsigned int rx, ry, rz;
+
+    if (getInputs(&rx, &ry, &rz) == 0) {
         return 0;
     }
 
-    int*** x;
-    int m, n;
-    x = (int***)malloc(p*sizeof(int**));
+    unsigned int p, l, c;
+    p = 2*rz + 1;
+    l = 2*ry + 1;
+    c = 2*rx + 1;
 
-    x[0] = (int**)malloc(p*l*sizeof(int*));
-    for (m = 1; m < p; m++) {
-        x[m] = x[m - 1] + l;
+    int*** x;
+    int i;
+    x = (int***)malloc(c*sizeof(int**));
+
+    x[0] = (int**)malloc(c*l*sizeof(int*));
+    for (i = 1; i < c; i++) {
+        x[i] = x[i - 1] + l;
     }
 
     x[0][0] = (int*)malloc(p*l*c*sizeof(int));
-    for (m = 1; m < p*l; m++) {
-        x[0][m] = x[0][m - 1] + c;
+    for (i = 1; i < c*l; i++) {
+        x[0][i] = x[0][i - 1] + p;
     }
 
-    
+    fillMatrix(x, rx, ry, rz);
+
+    printMatrix(x, p, l, c);
 
     free(x[0][0]);
     free(x[0]);
