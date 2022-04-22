2633a9929595ddaeb37ddefd92ad00a8ca0e4c6f
Glorijane Morais Medeiros <Lucas Morais Freire>
2022-04-21 23:04:51 -0300

    elipse fill v2

--- main.c
+++ main.c
@@ -1,39 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-short getInputs(int* pl, int* pc, int* pp) {
-    printf("digite o raio do elipsoide no eixo x: ");
-    scanf("%d", pl);
-    if (*pl < 1) {
-        printf("entrada invalida!");
-        return 0;
-    }
-    printf("\n");
-
-    printf("digite o raio do elipsoide no eixo y: ");
+void getInputs(int* pp, int* pl, int* pc) {
+    printf("digite o comprimento do paralelepipedo no eixo x: ");
     scanf("%d", pc);
     if (*pc < 1) {
         printf("entrada invalida!");
-        return 0;
+        exit(0);
     }
     printf("\n");
 
-    printf("digite o raio do elipsoide no eixo z: ");
+    printf("digite o comprimento do paralelepipedo no eixo y: ");
+    scanf("%d", pl);
+    if (*pl < 1) {
+        printf("entrada invalida!");
+        exit(0);
+    }
+    printf("\n");
+
+    printf("digite o comprimento do paralelepipedo no eixo z: ");
     scanf("%d", pp);
     if (*pp < 1) {
         printf("entrada invalida!");
-        return 0;
+        exit(0);
     }
     printf("\n");
-
-    return 1;
 }
 
 void printMatrix(int*** x, unsigned int p, unsigned int l, unsigned int c) {
     for (int k = 0; k < p; k++) {
         for (int j = 0; j < l; j++) {
             for (int i = 0; i < c; i++){
-                printf("%d", x[i][j][k]);
+                printf("%d ", x[i][j][k]);
             }
             printf("\n");
         }
@@ -41,22 +39,17 @@ void printMatrix(int*** x, unsigned int p, unsigned int l, unsigned int c) {
     }
 }
 
-void fillMatrix(int*** x, unsigned int rx, unsigned int ry, unsigned int rz) {
-
-    unsigned int p, l, c;
-    p = 2*rz + 1;
-    l = 2*ry + 1;
-    c = 2*rx + 1;
+void fillMatrix(int*** x, unsigned int p, unsigned int l, unsigned int c) {
 
     float u, v, w;
-    u = (float)rx;
-    v = (float)ry;
-    w = (float)rz;
+    u = (float)c;
+    v = (float)l;
+    w = (float)p;
 
     for (int k = 0; k < p; k++) {
         for (int j = 0; j < l; j++) {
             for (int i = 0; i < c; i++){
-                if ((i - u)*(i - u)/(u*u) + (j - v)*(j - v)/(v*v) + (k - w)*(k - w)/(w*w) <= 1.) {
+                if ((i - (u - 1)/2)*(i - (u - 1)/2)/((u - 1)/2*(u - 1)/2) + (j - (v - 1)/2)*(j - (v - 1)/2)/((v - 1)/2*(v - 1)/2) + (k - (w - 1)/2)*(k - (w - 1)/2)/((w - 1)/2*(w - 1)/2) <= 1.) {
                     x[i][j][k] = 1;
                 } else {
                     x[i][j][k] = 0;
@@ -95,16 +88,9 @@ void userLoop(int*** x, unsigned int p, unsigned int l, unsigned int c) {
 
 int main() {
 
-    unsigned int rx, ry, rz;
-
-    if (getInputs(&rx, &ry, &rz) == 0) {
-        return 0;
-    }
-
     unsigned int p, l, c;
-    p = 2*rz + 1;
-    l = 2*ry + 1;
-    c = 2*rx + 1;
+
+    getInputs(&p, &l, &c);
 
     int*** x;
     int i;
@@ -120,7 +106,7 @@ int main() {
         x[0][i] = x[0][i - 1] + p;
     }
 
-    fillMatrix(x, rx, ry, rz);
+    fillMatrix(x, p, l, c);
 
     userLoop(x, p, l, c);
 
